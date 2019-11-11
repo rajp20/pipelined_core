d75a1bd64a49fec550bbe9db29b6c177db0aaa65
Raj Patel <rajpatel0820@gmail.com>
2019-11-10 23:12:57 -0700

    initiated modules for IF and ID

---
+++ InstructionDecode.v
@@ -0,0 +1,9 @@
+module InstructionDecode(input             clk,
+			 input      [15:0] NPC_IF,
+			 input      [15:0] INST_IF,
+			 output reg [15:0] NPC_ID,
+			 output reg [15:0] REG1_ID
+			 output reg [15:0] REG2_ID
+			 output reg [4:0]  CTRL_ID);
+
+endmodule
--- InstructionFetch.v
+++ InstructionFetch.v
@@ -1,4 +1,10 @@
 module InstructionFetch(
-			input );
+			input 		  clk,
+			input      [15:0] PC,
+			input      [15:0] TARGET, 
+			input      [15:0] data_from_mem,
+			output reg [15:0] NPC_IF,
+			output reg [15:0] INST_IF);
+
    
 endmodule
