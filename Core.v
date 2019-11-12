b86860e911f71cb8a76c3379c01acbcee55e1ea3
Raj Patel <rajpatel0820@gmail.com>
2019-11-12 15:18:28 -0700

    core and inst fetch update

--- Core.v
+++ Core.v
@@ -1,6 +1,6 @@
 module Core(input             clk,
-	    input      [15:0] mem_to_core_data,
-	    output reg [23:0] core_to_mem_addr,
+	    input      [15:0] from_mem_data,
+	    output reg [15:0] to_mem_addr,
 	    output reg [15:0] core_to_mem_data,
 	    output reg        core_to_mem_write_enable);
 
@@ -13,21 +13,12 @@ module Core(input             clk,
    reg  [4:0]   write_index;
    reg  [23:0] 	write_data;
    reg 		write_enable;
- 		
 
-   RegisterFile _RegisterFile(.clk          (clk),
-			      .read_data_1  (read_data_1),
-			      .read_data_2  (read_data_2),
-			      .read_index_1 (read_index_1),
-			      .read_index_2 (read_index_2),
-			      .write_index  (write_index),
-			      .write_enable (write_enable),
-			      .write_data   (write_data));
-
-   reg [10:0] 	PC; 	
+   reg [15:0] 	PC;
    
    reg [15:0] 	NPC_IF;
    reg [15:0] 	TARGET;
+   reg  	TARGET_EN;
    reg [15:0] 	INST_IF;
 
    reg [15:0] 	NPC_ID;
@@ -43,7 +34,25 @@ module Core(input             clk,
    reg [15:0] 	DATA_MA;
    reg [4:0] 	CTRL_MA;
 
-   InstructionFetch _InstructionFetch();
+   initial      PC = 1'b0; 
+
+   RegisterFile _RegisterFile(.clk          (clk),
+			      .read_data_1  (read_data_1),
+			      .read_data_2  (read_data_2),
+			      .read_index_1 (read_index_1),
+			      .read_index_2 (read_index_2),
+			      .write_index  (write_index),
+			      .write_enable (write_enable),
+			      .write_data   (write_data));
+
+   InstructionFetch _InstructionFetch(.clk           (clk),
+				      .PC            (PC),
+				      .TARGET        (TARGET),
+				      .TARGET_EN     (TARGET_EN),
+				      .from_mem_data (from_mem_data),
+				      .to_mem_addr   (to_mem_addr),
+				      .NPC_IF        (NPC_IF),
+				      .INST_IF       (INST_IF));
 
    InstructionDecode _InstructionDecode();
 
--- InstructionFetch.v
+++ InstructionFetch.v
@@ -1,8 +1,9 @@
-module InstructionFetch(
-			input 		  clk,
-			input      [15:0] PC,
-			input      [15:0] TARGET, 
-			input      [15:0] data_from_mem,
+module InstructionFetch(input 		  clk,
+			input [15:0] 	  PC,
+			input [15:0] 	  TARGET,
+			input 		  TARGET_EN, 
+			input [15:0] 	  from_mem_data,
+			output reg [15:0] to_mem_addr,
 			output reg [15:0] NPC_IF,
 			output reg [15:0] INST_IF);
 
